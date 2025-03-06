9cc95820931fdd60d81a9b35717ff717375d1fba
deepseadiver5 <155144443+deepseadiver5@users.noreply.github.com>
2025-03-06 10:05:27 +0000

    Code added to JS script

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-let body = document.querySelector("body");
+const temp = document.querySelector("body");
+const welcome = document.querySelector("#welcome-message");
+welcome.color = 'blue';
+const button = document.querySelector("#button");
+button.onclick = pressed;
+
+function pressed(){
+    temp.style.backgroundColor = "green";
+    welcome.style.color = "yellow";
+    welcome.innerText = "Hello again!";
+}
